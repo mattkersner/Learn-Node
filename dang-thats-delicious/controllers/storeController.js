d0e7c25e4dfab0946c3a3f21475090c9e51305cf
mattkersner <mattkersner@gmail.com>
2018-12-01 23:19:33 -0500

    videos 9-14

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,13 +1,42 @@
-exports.myMiddleware = (req, res, next) => {
-	req.name = 'Matt';
-	res.cookie('name', 'Matt is cool', { maxAge: 9000000 });
-	if(req.name == 'Matt') {
-		throw Error('That is a great name');
-	}
-	next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-	console.log(req.name);
 	res.render('index');
+};
+
+exports.addStore = (req, res) => {
+	res.render('editStore', { title: '💩 Add Store' });
+};
+
+exports.createStore = async (req, res) => {
+	const store = await (new Store(req.body)).save();
+	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+	res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+	// 1. Query the db for a list of all stores
+  const stores = await Store.find();
+	res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+	// 1. find the store given the id
+	const store = await Store.findOne({ _id: req.params.id });
+
+	// 2. confirm they are the owner of the store
+	// 3. render out the edit form so the user can update their store
+	res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+	// find and update the store
+	const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+		new: true, //return the new store instead of the old one
+		runValidators: true
+	}).exec();
+	req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`)
+	// redirect them to the store and tell them it worked
+	res.redirect(`/stores/${store._id}/edit`);
 };
